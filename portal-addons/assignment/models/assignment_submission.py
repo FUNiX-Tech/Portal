d09a4f888ea9e4eabc36be7c198e506bece60da1
uuuuv <vuntafx17889@funix.edu.vn>
2023-10-31 06:30:37 +0700

    [FEAT] add submit button and logic for submitting grade button

--- portal-addons/assignment/models/assignment_submission.py
+++ portal-addons/assignment/models/assignment_submission.py
@@ -6,7 +6,7 @@ class AssignmentSubmission(models.Model):
     _name = 'assignment_submission'
     _description = 'assignment_submission'
 
-    student = fields.Many2one('portal.student', string="Học viên") 
-    assignment = fields.Many2one("assignment", string="Assignment")
-    submission_url = fields.Char(string="Submission Url") 
-    criteria_responses = fields.One2many("assignment_criterion_response", inverse_name="submission", string="Các tiêu chí")
+    student = fields.Many2one('portal.student', string="Học viên", readonly=True) 
+    assignment = fields.Many2one("assignment", string="Assignment", readonly=True) 
+    submission_url = fields.Char(string="Submission Url", readonly=True) 
+    criteria_responses = fields.One2many("assignment_criterion_response", inverse_name="submission", string="Các tiêu chí") 
