86442ed656f7a53759620d3102946b705ef8e04e
Admin <hoangbnmfx19875@funix.edu.vn>
2023-11-09 12:49:57 +0700

    feat: complete service key module

--- portal-addons/service_key/models/service_key.py
+++ portal-addons/service_key/models/service_key.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 # Service Key Config
@@ -7,25 +7,12 @@ class ServiceKeyConfiguration(models.Model):
     _description = "Service Key Configuration"
 
     name = fields.Char(string="Service Name", required=True)
-    key_ids = fields.One2many("service_key", "config_id", string="API Keys")
+    api_key = fields.Char(string="API Key", required=True)
 
     # Method get api key by name of service
     def get_api_key_by_service_name(self, service_name):
-        key_record = self.key_ids.filtered(
-            lambda key: key.name == service_name
-        )
+        key_record = self.search([("name", "=", service_name)])
         if key_record:
             return key_record.api_key
         else:
             return False
-
-
-# Service Key
-class ServiceKey(models.Model):
-    _name = "service_key"
-    _description = "Service Key"
-
-    config_id = fields.Many2one(
-        "service_key_configuration", string="Service", ondelete="cascade"
-    )
-    api_key = fields.Char(string="API Key", required=True)
