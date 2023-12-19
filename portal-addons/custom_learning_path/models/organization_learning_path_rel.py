4e626c7320dc12a097c728ba0a84117bbb51fd00
khoansfx <khoansfx20189@funix.edu.vn>
2023-12-19 21:36:44 +0700

    adjust: remove editor and featured editor field in custom learning path

--- portal-addons/custom_learning_path/models/organization_learning_path_rel.py
+++ portal-addons/custom_learning_path/models/organization_learning_path_rel.py
@@ -33,45 +33,45 @@ class OrgLearningPath(models.Model):
         domain="[('student_organization_student_ids', '=', organization_id)]",
     )
 
-    editor_ids = fields.Many2many(
-        comodel_name="portal.student",
-        relation="editor_org_learning_path_rel",
-        column1="org_learning_path_ids",
-        column2="student_ids",
-        string="Editors",
-        domain="[('student_organization_student_ids', '=', organization_id)]",
-    )
+    # editor_ids = fields.Many2many(
+    #     comodel_name="portal.student",
+    #     relation="editor_org_learning_path_rel",
+    #     column1="org_learning_path_ids",
+    #     column2="student_ids",
+    #     string="Editors",
+    #     domain="[('student_organization_student_ids', '=', organization_id)]",
+    # )
 
-    featured_editor_ids = fields.Many2many(
-        comodel_name="portal.student",
-        relation="org_learning_path_featured_editor_rel",
-        column1="org_learning_path_ids",
-        column2="student_ids",
-        string="Featured Editors",
-        domain="[('student_organization_student_ids', '=', organization_id)]",
-    )
+    # featured_editor_ids = fields.Many2many(
+    #     comodel_name="portal.student",
+    #     relation="org_learning_path_featured_editor_rel",
+    #     column1="org_learning_path_ids",
+    #     column2="student_ids",
+    #     string="Featured Editors",
+    #     domain="[('student_organization_student_ids', '=', organization_id)]",
+    # )
 
 
 class Student(models.Model):
     _inherit = "portal.student"
 
-    # Many2many field for paths where the student is an editor
-    editor_org_learning_path_ids = fields.Many2many(
-        comodel_name="organization_learning_path",
-        relation="editor_org_learning_path_rel",
-        column1="student_ids",
-        column2="org_learning_path_ids",
-        string="Edited Custom Learning Paths",
-    )
+    # # Many2many field for paths where the student is an editor
+    # editor_org_learning_path_ids = fields.Many2many(
+    #     comodel_name="organization_learning_path",
+    #     relation="editor_org_learning_path_rel",
+    #     column1="student_ids",
+    #     column2="org_learning_path_ids",
+    #     string="Edited Custom Learning Paths",
+    # )
 
-    # Many2many field for paths where the student is a featured editor
-    featured_editor_org_learning_path_ids = fields.Many2many(
-        comodel_name="organization_learning_path",
-        relation="org_learning_path_featured_editor_rel",
-        column1="student_ids",
-        column2="org_learning_path_ids",
-        string="Featured Editor Learning Paths",
-    )
+    # # Many2many field for paths where the student is a featured editor
+    # featured_editor_org_learning_path_ids = fields.Many2many(
+    #     comodel_name="organization_learning_path",
+    #     relation="org_learning_path_featured_editor_rel",
+    #     column1="student_ids",
+    #     column2="org_learning_path_ids",
+    #     string="Featured Editor Learning Paths",
+    # )
 
     org_learning_path_ids = fields.One2many(
         comodel_name="organization_learning_path",
